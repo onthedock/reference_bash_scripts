a718282aa6196ea6715387fb2ae4b297f6866901
Xavi Aznar <xavi.aznar@outlook.com>
2021-10-08 19:10:18 +0200

    Naive tests
    Better than nothing ;)

---
+++ request_user_input/test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source ./request.sh
+
+# No custom message, no default value
+echo "Output: (no default value) [] ?" 
+echo "Value: "
+myVar1=$(request)
+echo "$myVar1"
+
+# Custom message, no default value
+echo "Please, enter a value (no default value) [] ?"
+echo "Value: "
+myVar2=$(request "Please, enter a value")
+echo "$myVar2"
+
+# No custom message,  default value
+echo " [Default Value] ?"
+echo "Value: Default Value"
+myVar3=$(request "" "Default Value")
+echo "$myVar3"
+
+# Cstom message,  default value
+echo "Please, enter a value [Default Value] ?"
+echo "Value: Default Value"
+myVar4=$(request "Please, enter a value" "Default Value")
+echo "$myVar4"
