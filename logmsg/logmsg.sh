2d36727ed1a1d5b7d0341e2050fa143afe4709c5
Xavi Aznar <xavi.aznar@outlook.com>
2021-10-08 19:20:45 +0200

    Moves (naive) tests to its own file

--- logmsg/logmsg.sh
+++ logmsg/logmsg.sh
@@ -94,12 +94,3 @@ log () {
         fi
     fi
 }
-
-logConfig --make-temp-dir --log-level 0
-
-log "it's working"
-log "information: it works" INFO
-log "don't know what it is" PEPE
-log "it's not working :(" ERROR
-log "uhm, probably it's nothing to worry about" WARN
-log "need everything you've got" DEBUG
