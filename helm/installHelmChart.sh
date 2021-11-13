59443c3121001198de3e4fe7892b0c051b25c355
Xavi Aznar <xavi.aznar@outlook.com>
2021-11-13 22:46:40 +0100

    Instala una helm chart desde un repo
    Antes de instalar la chart, comprueba si ya está instalada.
    En ese caso no la
    instala.

---
+++ helm/installHelmChart.sh
@@ -0,0 +1,15 @@
+function installHelmChart {
+    helmChart="$1"
+    helmRepoChart="$2"
+    chartNamespace="$3"
+
+    checkRelease=$(helm status $helmChart --namespace $chartNamespace 2>/dev/null| grep -i status | awk '{ print $2 }')
+
+    if [ "$checkRelease" != "deployed" ]
+    then
+        echo "...Installing longhorn"
+        helm install $helmChart $helmRepoChart --namespace $chartNamespace --create-namespace
+    else
+        echo "... $helmChart is already installed in the namespace $chartNamespace"
+    fi
+}
