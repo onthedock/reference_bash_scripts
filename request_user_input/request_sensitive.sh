1ad19848058daa6865c79850a8429e7d4357f36b
Xavi <xavi.aznar@outlook.com>
2022-01-22 10:25:03 +0100

    Incluye `-s` que no hace "echo" de lo introducido por el usuario

---
+++ request_user_input/request_sensitive.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# request_sensitive "Prompt text" "defaultValue" -s|--sensitive
+# With -s|--sensitive, pass the `-s` option to `read` so there is
+# no echo on the terminal (useful for passwords, etc)
+
+parse_cli_args () {
+    PARAMS=()
+
+    while (( "$#" ))
+    do
+        case "$1" in
+            -s|--sensitive)
+                SENSITIVE="yes"
+                shift
+                ;;
+            -*|--*=) # Unsupported flags
+                echo "Error: Unsupported flag $1" >&2
+                exit 1
+                ;;
+            *) # Preserve positional arguments
+                PARAMS+=("$1")
+                shift
+                ;;
+        esac
+    done
+
+    # Set positional arguments in their proper place
+    eval set -- "$PARAMS"
+}
+
+function request () {
+    parse_cli_args "$@"
+
+    msg=${PARAMS[0]}
+
+    if [ ${#PARAMS[@]} -lt 2 ]
+    then
+        msg+=" (No default value)"
+    else
+        defaultValue="${PARAMS[1]}"
+        msg+=" (default: ${defaultValue})"
+    fi
+
+    if [ "${SENSITIVE}" == "yes" ]
+    then
+        read -s -p "$msg : " userinput
+    else
+        read -p "$msg : " userinput
+    fi
+
+    if [ -z "${userinput}" ]
+    then
+        userinput=$defaultValue
+    fi
+
+    echo ${userinput}
+}
