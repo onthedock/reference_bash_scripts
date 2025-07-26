fa6cd914cbb2cd0ea1c52aec6b6bd32977accea2
Xavi Aznar <xavier.aznar@seat.es>
2025-07-26 06:01:01 +0000

    feat: add a newline character to file if it doesn't end with one

---
+++ add_newline/add_newline.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+add_newline() {
+    # add_newline checks the file for a newline character at the end of the file and appends it to the file if it's not present.
+    # In the UNIX/Linux world, a text file is defined as a sequence of lines, each ending with a newline character (\n).
+    # Without a newline, the last line is considered incomplete.
+    # Many tools and scripts expect this convention, and omitting it can lead to unexpected behavior or errors.
+    local filename
+    filename="${1}"
+    filename=${filename:?Cannot be empty}
+    if [[ ! -s $filename ]]; then errcho "'$filename' cannot be an empty file'"; exit 1; fi
+    if [[ -n "$(tail -c 1 <"$filename")" ]]; then
+        cp "$filename" "${filename}.bkp"    # backup original file (just in case) 
+        printf "\n" >> "$filename"
+    fi
+}
